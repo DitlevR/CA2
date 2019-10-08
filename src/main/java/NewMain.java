d5b57d0018d5c980f9c733a53f8076043cd7f070
ITE17 <cph-lb270@cphbusiness.dk>
2019-10-08 22:14:46 +0200

    a

--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -35,158 +35,19 @@ public class NewMain {
             "jdbc:mysql://localhost:3307/CA2",
             "dev",
             "ax2",
-            EMF_Creator.Strategy.DROP_AND_CREATE); //DROP_AND_CREATE
+            EMF_Creator.Strategy.CREATE); //DROP_AND_CREATE
+    
+     public static final PersonFacade FACADE =  PersonFacade.getFacadeExample(EMF);
 
     public static void main(String[] args) {
         Persistence.generateSchema("pu", null);
         EntityManager em = EMF.createEntityManager();
 
-        Person p1 = new Person("fuglen@hotmail.com", "Hanne", "Svendsen");
-        Person p2 = new Person("heavymetal@outlook.com", "Thorsen", "Odinsen");
-        Person p3 = new Person("himmelen@outlook.com", "Iben", "Asgersen");
-        Person p4 = new Person("treetrees@hotmail.com", "Benny", "Baldersen");
-        Person p5 = new Person("ørnen@hotmail.com", "Helge", "Svendsen");
-        Person p6 = new Person("celticpunk@outlook.com", "Thomsen", "Odinsen");
-        Person p7 = new Person("jotunheim@outlook.com", "Ibensen", "Asgersen");
-        Person p8 = new Person("bushes@hotmail.com", "Soren", "Baldersen");
-        Person p9 = new Person("pingvin@hotmail.com", "Janne", "Svendsen");
-        Person p10 = new Person("kolibri@outlook.com", "Anne", "Odinsen");
-        Person p11 = new Person("mumle@outlook.com", "Ibensen", "Asgersen");
-        Person p12 = new Person("lars@hotmail.com", "Benny", "Baldersen");
-        Person p13 = new Person("hjemlos@google.com", "Hjemlos", "Vandrer");
 
-        Phone ph1 = new Phone("12345678", "Til alarmcentralen");
-        Phone ph2 = new Phone("99999999", "Til himmelen");
-        Phone ph3 = new Phone("77777777", "Til jesus");
-        Phone ph4 = new Phone("88888888", "Til Leasy");
-        Phone ph5 = new Phone("123123456", "Til brandstationen");
-        Phone ph6 = new Phone("99999999", "Til hospitalet");
-        Phone ph7 = new Phone("77777777", "Til borgmesteren");
-        Phone ph8 = new Phone("88888888", "Til Ludvif");
-        Phone ph9 = new Phone("12345678", "Til statsministeren");
-        Phone ph10 = new Phone("99999999", "Til Valhalla");
-        Phone ph11 = new Phone("77777777", "Til Thor");
-        Phone ph12 = new Phone("88888888", "Til Ly");
-
-        Hobby h1 = new Hobby("Svømning", "man svømmer");
-        Hobby h2 = new Hobby("Klatring", "man klatrer");
-        Hobby h3 = new Hobby("Tegning", "man tegner");
-        Hobby h4 = new Hobby("Sang", "man synger");
-
-//        CityInfo ci1 = new CityInfo("1234", "Brovst");
-//        CityInfo ci2 = new CityInfo("4321", "Aabenraa");
-//        CityInfo ci3 = new CityInfo("87654", "Allerød");
-        Address a1 = new Address("Parkvej", "Der ligger en mose i området", "1234", "Brovst");
-        Address a2 = new Address("Jagtvej", "ungdomshuset lå engang her", "1234", "Brovst");
-        Address a3 = new Address("Kongevejen", "Går over Geels bakken", "2830", "Lyngby");
-        Address a4 = new Address("Nørrebrogade", "Her ligger 3 spa", "4600", "København");
-        Address a5 = new Address("Holstensvej", "Meget fin", "0001", "Verdens ende");
-        Address a6 = new Address("Nordvej", "Dette sted er koldt", "1000", "Højt oppe");
-        Address a7 = new Address("Bomgade", "Udkig til Japan", "2830", "Virum");
-        Address a8 = new Address("Okostræde", "Frej helligdom i nærheden", "7777", "Anholt");
-
-        ph1.setP(p1);
-        ph2.setP(p1);
-        ph3.setP(p2);
-        ph4.setP(p2);
-        ph5.setP(p3);
-        ph6.setP(p3);
-        ph7.setP(p4);
-        ph8.setP(p4);
-        ph9.setP(p5);
-        ph10.setP(p5);
-        ph11.setP(p6);
-        ph12.setP(p6);
-
-        p1.setA(a1);
-        p2.setA(a2);
-        p3.setA(a3);
-        p4.setA(a4);
-        p5.setA(a5);
-        p6.setA(a6);
-        p7.setA(a7);
-        p8.setA(a8);
-        p9.setA(a1);
-        p10.setA(a2);
-        p11.setA(a3);
-        p12.setA(a4);
-
-        p1.setHobby(h1);
-        p2.setHobby(h1);
-        p1.setHobby(h2);
-        p3.setHobby(h3);
-        p3.setHobby(h2);
-        p4.setHobby(h4);
-        p5.setHobby(h4);
-        p6.setHobby(h1);
-        p7.setHobby(h1);
-        p8.setHobby(h2);
-        p9.setHobby(h3);
-        p10.setHobby(h2);
-        p12.setHobby(h4);
-        p13.setHobby(h4);
-        p7.setHobby(h2);
-        p8.setHobby(h1);
-        p10.setHobby(h4);
-        p9.setHobby(h2);
-        p11.setHobby(h2);
-        p8.setHobby(h4);
-        p1.setHobby(h4);
-
-//        a1.setCi(ci1);
-//        a2.setCi(ci2);
-//        a3.setCi(ci3);
-//        a4.setCi(ci1);
-//        a5.setCi(ci2);
-//        a6.setCi(ci3);
-//        a7.setCi(ci1);
-//        a8.setCi(ci2);
         try {
             em.getTransaction().begin();
 
-//            em.persist(ci1);
-//            em.persist(ci2);
-//            em.persist(ci3);
-            em.persist(a1);
-            em.persist(a2);
-            em.persist(a3);
-            em.persist(a4);
-            em.persist(a5);
-            em.persist(a6);
-            em.persist(a7);
-            em.persist(a8);
 
-            em.persist(p1);
-            em.persist(p2);
-            em.persist(p3);
-            em.persist(p4);
-            em.persist(p5);
-            em.persist(p6);
-            em.persist(p7);
-            em.persist(p8);
-            em.persist(p9);
-            em.persist(p10);
-            em.persist(p12);
-            em.persist(p11);
-            em.persist(p13);
-
-            em.persist(h1);
-            em.persist(h2);
-            em.persist(h3);
-            em.persist(h4);
-
-            em.persist(ph1);
-            em.persist(ph2);
-            em.persist(ph3);
-            em.persist(ph4);
-            em.persist(ph5);
-            em.persist(ph6);
-            em.persist(ph7);
-            em.persist(ph8);
-            em.persist(ph9);
-            em.persist(ph10);
-            em.persist(ph11);
-            em.persist(ph12);
 
             em.getTransaction().commit();
         } finally {
