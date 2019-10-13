83fcc92e419c61c15834587528e26e4e305c245d
ITE17 <cph-lb270@cphbusiness.dk>
2019-10-13 19:54:11 +0200

    ændrede i index.js

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -102,11 +102,11 @@ const lNameNewPerson = document.getElementById("lnameInput");
 const emailNewPerson = document.getElementById("emailInput");
 const addressIdPerson = document.getElementById("addressIdInput");
 function addUser() {
-    const newPerson = { body: JSON.stringify({
+    const data = { 
         fName: fNameNewPerson.value,
         lName: lNameNewPerson.value,
         email: emailNewPerson.value
-    })};
+    };
 
     const other_params = {
         headers: {"content-type": "application/json; charset=UTF-8"},
@@ -115,7 +115,7 @@ function addUser() {
         mode: "cors"
     };
 
-    fetch(allUrl, other_params)
+    fetch(allUrl + "add/" + addressIdPerson, other_params)
             .then(function (response) {
                 if (response.ok) {
                     return response.json();
@@ -149,27 +149,6 @@ function addUser() {
 //    document.getElementById("addressIdInput").value = "";
 //    document.getElementById("emailInput").value = "";
 }
-
-const other_params = {
-    headers: {"content-type": "application/json; charset=UTF-8"},
-    body: data,
-    method: "POST",
-    mode: "cors"
-};
-
-fetch(url, other_params)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error("Could not reach the API: " + response.statusText);
-            }
-        }).then(function (data) {
-    document.getElementById("message").innerHTML = data.encoded;
-}).catch(function (error) {
-    document.getElementById("message").innerHTML = error.message;
-});
-return true;
 
 function showSprints() {
     document.getElementById("root").innerHTML =
